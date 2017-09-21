ccdb03989676f182d7c8ccc91e69662eac4931d1
Justin Parrondo <justinparrondo@gmail.com>
2017-09-21 01:11:33 -0400

    Starts main with a scanner variable and creates getRandomNumber(start, stop) method

--- activity.java
+++ activity.java
@@ -1,1 +1,16 @@
 // Initial commit
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class activity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+    }
+    public static int getRandomNumber(int start, int stop) {
+        int randomNumber = (int) (Math.random() * (stop - start) + 1);
+        return randomNumber;
+    }
+}
